a150ca9adc190a15fe1564ac681036d997f6c011
EntropicProblems <EntropicProblems@github.com>
2024-03-27 13:09:06 -0400

    External variable test

--- cprojects/wordlong.c
+++ cprojects/wordlong.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #define MAXLINE 1000
 
-void copy(char to[], char from[]);
-int getlinee(char line[], int maxline);
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+void copy(void);
+int getlinee(void);
 int main(int argc, char** argv)
 {
 	int len;
-	int max;
-	char line[MAXLINE];
-	char longest[MAXLINE];
-	max = 0;
-	while((len = getlinee(line, MAXLINE)) > 0)
+	extern int max;
+	extern char longest[];
+	while((len = getlinee()) > 0)
 	{
 		if(len > max)
 		{
-			max = len;
-			copy(longest, line);
+			max = len - 1;
+			copy();
 		}
 	}
 	if(max > 0)
@@ -24,17 +25,19 @@ int main(int argc, char** argv)
 	}
 	return 0;
 }
-void copy(char to[], char from[])
+void copy(void)
 {
+	extern char line[], longest[];
 	int i;
 	i = 0;
-	while ((to[i] = from[i]) != '\0')
+	while ((longest[i] = line[i]) != '\0')
 		++i;
 }
-int getlinee(char line[], int maxline)
+int getlinee(void)
 {
+	extern char line[];
 	int c,i;
-	for(i = 0; (i < maxline) && ((c = getchar()) != EOF) && (c != '\n'); ++i)
+	for(i = 0; (i < MAXLINE) && ((c = getchar()) != EOF) && (c != '\n'); ++i)
 		line[i] = c;
 	if(c == '\n')
 	{
