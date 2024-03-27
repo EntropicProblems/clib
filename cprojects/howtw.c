0c809b92e0feaaaf73d9ffc9942e3f1a058a98d6
EntropicProblems <EntropicProblems@github.com>
2024-03-27 13:37:12 -0400

    Condensed

--- cprojects/howtw.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	double nc;
-
-	for(nc = 0; getchar() != '\n'; ++nc)
-		;
-	printf("%.0lf\n", nc);
-}
--- cprojects/tryfirst.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(int args)
-{
-printf("hello, world\n");
-}
